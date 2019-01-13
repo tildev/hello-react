39b7d1c86a0729cef53b836087a265d92a44d2d2
tildev <tildevelove@gmail.com>
2019-01-13 20:21:51 +0900

    add react init code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; // var React = require('react'); var Component = React.Component;
+
+class App extends Component {
+  render() {
+   return (
+     <div>
+      <h1>React hello!</h1>
+      <h2>Hello!!!</h2>
+     </div>
+   );
+  }
+}
+
+export default App;
