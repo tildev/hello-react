d374dc9641daf7214a044db5bc7eabcc3c158d19
tildev <tildevelove@gmail.com>
2019-01-13 20:38:31 +0900

    add react code step1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'; // var React = require('react'); var Component = React.Component;
+import './App.css'
 
 class App extends Component {
   render() {
     const text ="hello! :)"
     const condition = true;
+    const style ={
+      backgroundColor:'blue',
+      border: '1px solid black',
+      height: Math.round(Math.random()*300) + 50,
+      width: Math.round(Math.random()*300) + 50,
+      webkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
    return (
-     <div>
+     <div className="my-div">
+     {/*요소 밖 주석 */}
       <h1>React hello!</h1>
       <h2>{text}</h2>
       {
-        condition? 'true' : 'false'
+        condition && 'show' 
       }
+      <div style={style}
+        // self-closed 태그에서만 작동하는 주석
+        /* 이렇게도 작성 가능 */
+      />
      </div>
    );
   }
